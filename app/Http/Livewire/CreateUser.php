f3fdcb85a881d5855f09338fa8f8396802acc55e
Ryan Aunur <ryandevstudio@gmail.com>
2020-09-27 05:27:42 +0700

    create(livewire): create user
    Livewire component handle create and update user

---
+++ app/Http/Livewire/CreateUser.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+
+class CreateUser extends Component
+{
+    public $user;
+    public $userId;
+    public $action;
+    public $button;
+
+    protected function getRules()
+    {
+        $rules = ($this->action == "updateUser") ? [
+            'user.email' => 'required|email|unique:users,email,' . $this->userId
+        ] : [
+            'user.password' => 'required|min:8|confirmed',
+            'user.password_confirmation' => 'required' // livewire need this
+        ];
+
+        return array_merge([
+            'user.name' => 'required|min:3',
+            'user.email' => 'required|email|unique:users,email'
+        ], $rules);
+    }
+
+    public function createUser ()
+    {
+        $this->resetErrorBag();
+        $this->validate();
+
+        User::create($this->user);
+
+        $this->emit('saved');
+        $this->reset('user');
+    }
+
+    public function updateUser ()
+    {
+        $this->resetErrorBag();
+        $this->validate();
+
+        User::query()
+            ->where('id', $this->userId)
+            ->update($this->user);
+
+        $this->emit('saved');
+    }
+
+    public function mount ()
+    {
+        if (!!$this->userId) {
+            $user = User::find($this->userId);
+
+            $this->user = [
+                "name" => $user->name,
+                "email" => $user->email,
+            ];
+        }
+
+        $this->button = create_button($this->action, "User");
+    }
+
+    public function render()
+    {
+        return view('livewire.create-user');
+    }
+}
---
+++ resources/views/livewire/create-user.blade.php
@@ -0,0 +1,54 @@
+<div id="form-create">
+    <x-jet-form-section :submit="$action" class="mb-4">
+        <x-slot name="title">
+            {{ __('User') }}
+        </x-slot>
+
+        <x-slot name="description">
+            {{ __('Lengkapi data berikut dan submit untuk membuat data user baru') }}
+        </x-slot>
+
+        <x-slot name="form">
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="name" value="{{ __('Nama') }}" />
+                <small>Nama Lengkap Akun</small>
+                <x-jet-input id="name" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.name" />
+                <x-jet-input-error for="user.name" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" type="text" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.email" />
+                <x-jet-input-error for="user.email" class="mt-2" />
+            </div>
+
+            @if ($action == "createUser")
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <small>Minimal 8 karakter</small>
+                <x-jet-input id="password" type="password" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.password" />
+                <x-jet-input-error for="user.password" class="mt-2" />
+            </div>
+
+            <div class="form-group col-span-6 sm:col-span-5">
+                <x-jet-label for="password_confirmation" value="{{ __('Konfirmasi Password') }}" />
+                <small>Minimal 8 karakter</small>
+                <x-jet-input id="password_confirmation" type="password" class="mt-1 block w-full form-control shadow-none" wire:model.defer="user.password_confirmation" />
+                <x-jet-input-error for="user.password_confirmation" class="mt-2" />
+            </div>
+            @endif
+        </x-slot>
+
+        <x-slot name="actions">
+            <x-jet-action-message class="mr-3" on="saved">
+                {{ __($button['submit_response']) }}
+            </x-jet-action-message>
+
+            <x-notify-message on="saved" type="success" :message="__($button['submit_response_notyf'])" />
+
+            <x-jet-button>
+                {{ __($button['submit_text']) }}
+            </x-jet-button>
+        </x-slot>
+    </x-jet-form-section>
+</div>
