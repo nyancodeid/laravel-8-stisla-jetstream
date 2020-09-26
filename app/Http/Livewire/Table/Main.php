e4d748294c0c484ee2e77afbc8cc36176eff485d
Ryan Aunur <ryandevstudio@gmail.com>
2020-09-27 05:25:50 +0700

    create: main data table livewire component

---
+++ app/Http/Livewire/Table/Main.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Livewire\Table;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Main extends Component
+{
+    use WithPagination;
+
+    public $model;
+    public $name;
+
+    public $perPage = 10;
+    public $sortField = "id";
+    public $sortAsc = false;
+    public $search = '';
+
+    protected $listeners = [ "deleteItem" => "delete_item" ];
+
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortAsc = ! $this->sortAsc;
+        } else {
+            $this->sortAsc = true;
+        }
+
+        $this->sortField = $field;
+    }
+
+    public function get_pagination_data ()
+    {
+        switch ($this->name) {
+            case 'user':
+                $users = $this->model::search($this->search)
+                    ->orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc')
+                    ->paginate($this->perPage);
+
+                return [
+                    "view" => 'livewire.table.user',
+                    "users" => $users,
+                    "data" => array_to_object([
+                        'href' => [
+                            'create_new' => route('user.new'),
+                            'create_new_text' => 'Buat User Baru',
+                            'export' => '#',
+                            'export_text' => 'Export'
+                        ]
+                    ])
+                ];
+                break;
+
+            default:
+                # code...
+                break;
+        }
+    }
+
+    public function delete_item ($id)
+    {
+        $data = $this->model::find($id);
+
+        if (!$data) {
+            $this->emit("deleteResult", [
+                "status" => false,
+                "message" => "Gagal menghapus data " . $this->name
+            ]);
+            return;
+        }
+
+        $data->delete();
+        $this->emit("deleteResult", [
+            "status" => true,
+            "message" => "Data " . $this->name . " berhasil dihapus!"
+        ]);
+    }
+
+    public function render()
+    {
+        $data = $this->get_pagination_data();
+
+        return view($data['view'], $data);
+    }
+}
