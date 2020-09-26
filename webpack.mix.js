e2aebd5942d2529ad989650f0e1d6c84652eecc2
Ryan Aunur <ryandevstudio@gmail.com>
2020-09-27 05:24:03 +0700

    update: split up tailwind from app
    To make sure not rebuild tailwind every app.css changed

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+    .postCss('resources/css/app.css', 'public/css')
+    .postCss('resources/css/tailwind.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
     ]);
