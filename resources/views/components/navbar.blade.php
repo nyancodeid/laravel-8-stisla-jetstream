37a432a648488d252d1d8e97c9af21e9033ec8b3
Ryan Aunur <ryandevstudio@gmail.com>
2020-09-27 05:22:16 +0700

    create(component): sidebar and navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,40 @@
+@php
+$user = auth()->user();
+@endphp
+
+<div class="navbar-bg"></div>
+<nav class="navbar navbar-expand-lg main-navbar">
+    <form class="form-inline mr-auto">
+        <ul class="navbar-nav mr-3">
+            <li><a href="#" data-turbolinks="false" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+        </ul>
+        <h1 class="font-weight-bold text-2xl text-white">{{ config('app.name', 'Laravel') }}</h1>
+    </form>
+    <ul class="navbar-nav navbar-right">
+        <li class="dropdown"><a href="#" data-turbolinks="false" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            @if (!is_null($user))
+                <div class="d-sm-none d-lg-inline-block">Hi, {{ $user->name }}</div></a>
+            @else
+                <div class="d-sm-none d-lg-inline-block">Hi, Welcome</div></a>
+            @endif
+            <div class="dropdown-menu dropdown-menu-right">
+                <a href="/user/profile" class="dropdown-item has-icon">
+                    <i class="far fa-user"></i> Profile
+                </a>
+                @if (request()->get('is_admin'))
+                <a href="/setting" class="dropdown-item has-icon">
+                    <i class="fas fa-cog"></i> Setting
+                </a>
+                @endif
+                <div class="dropdown-divider"></div>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger" onclick="event.preventDefault();this.closest('form').submit();">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                </form>
+            </div>
+        </li>
+    </ul>
+</nav>
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,65 @@
+@php
+$links = [
+    [
+        "href" => "dashboard",
+        "text" => "Dashboard",
+        "is_multi" => false,
+    ],
+    [
+        "href" => [
+            [
+                "section_text" => "User",
+                "section_list" => [
+                    ["href" => "user", "text" => "Data User"],
+                    ["href" => "user.new", "text" => "Buat User"]
+                ]
+            ]
+        ],
+        "text" => "User",
+        "is_multi" => true,
+    ],
+];
+$navigation_links = array_to_object($links);
+@endphp
+
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="{{ route('dashboard') }}">Dashboard</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="{{ route('dashboard') }}">
+                <img class="d-inline-block" width="32px" height="30.61px" src="" alt="">
+            </a>
+        </div>
+        @foreach ($navigation_links as $link)
+        <ul class="sidebar-menu">
+            <li class="menu-header">{{ $link->text }}</li>
+            @if (!$link->is_multi)
+            <li class="{{ Request::routeIs($link->href) ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route($link->href) }}"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            </li>
+            @else
+                @foreach ($link->href as $section)
+                    @php
+                    $routes = collect($section->section_list)->map(function ($child) {
+                        return Request::routeIs($child->href);
+                    })->toArray();
+
+                    $is_active = in_array(true, $routes);
+                    @endphp
+
+                    <li class="dropdown {{ ($is_active) ? 'active' : '' }}">
+                        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-chart-bar"></i> <span>{{ $section->section_text }}</span></a>
+                        <ul class="dropdown-menu">
+                            @foreach ($section->section_list as $child)
+                                <li class="{{ Request::routeIs($child->href) ? 'active' : '' }}"><a class="nav-link" href="{{ route($child->href) }}">{{ $child->text }}</a></li>
+                            @endforeach
+                        </ul>
+                    </li>
+                @endforeach
+            @endif
+        </ul>
+        @endforeach
+    </aside>
+</div>
