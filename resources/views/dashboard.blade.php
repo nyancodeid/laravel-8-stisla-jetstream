0f654a77b724c3e2a1b7efc47f2b95be8d9d7fde
Ryan Aunur <ryandevstudio@gmail.com>
2020-09-27 05:20:59 +0700

    update: dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+    <x-slot name="header_content">
+        <h1>Dashboard</h1>
+        <div class="section-header-breadcrumb">
+            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+            <div class="breadcrumb-item"><a href="#">Layout</a></div>
+            <div class="breadcrumb-item">Default Layout</div>
+        </div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
+    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <x-jet-welcome />
     </div>
 </x-app-layout>
