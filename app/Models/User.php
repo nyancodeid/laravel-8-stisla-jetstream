a03ef12fad28387fafe7c39c4571393fb9f508ce
Ryan Aunur <ryandevstudio@gmail.com>
2020-10-01 18:34:14 +0700

    update: set password attribute. fix #1

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
@@ -59,6 +60,12 @@ class User extends Authenticatable
         'profile_photo_url',
     ];
 
+    public function setPasswordAttribute($password)
+    {
+        if ( !empty($password) ) {
+            $this->attributes['password'] = Hash::make($password);
+        }
+    }
     /**
      * Search query in multiple whereOr
      */
